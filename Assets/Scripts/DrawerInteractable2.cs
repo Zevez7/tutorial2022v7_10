0658953642c26b22f63449d532b9bfd3df61e97e
Zevez7 <datguy7@gmail.com>
2024-07-16 22:57:24 -0400

    unity tutorial with VR

---
+++ Assets/Scripts/DrawerInteractable2.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DrawerInteractable2 : XRGrabInteractable
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Transform drawerTransform;
+
+    [SerializeField]
+    XRSocketInteractor keySocket;
+
+    [SerializeField]
+    bool isLocked;
+
+    private Transform parentTransform;
+    private const string defaultLayer = "Default";
+    private const string grabLayer = "Grab";
+    private bool isGrabbed;
+
+    private Vector3 limitPosition;
+
+    [SerializeField]
+    float drawerlimitz = 0.8f;
+
+    [SerializeField]
+    private Vector3 limitDistances = new Vector3(.02f, .02f, 0);
+
+    void Start()
+    {
+        if (keySocket != null)
+        {
+            keySocket.selectEntered.AddListener(OnDrawerUnlocked);
+            keySocket.selectExited.AddListener(OnDrawerLocked);
+        }
+        parentTransform = transform.parent.transform;
+        limitPosition = drawerTransform.localPosition;
+    }
+
+    private void OnDrawerUnlocked(SelectEnterEventArgs arg0)
+    {
+        isLocked = false;
+        Debug.Log("Drawer Unlocked");
+    }
+
+    private void OnDrawerLocked(SelectExitEventArgs arg0)
+    {
+        isLocked = true;
+        Debug.Log("Drawer Locked");
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        if (!isLocked)
+        {
+            transform.SetParent(parentTransform);
+            isGrabbed = true;
+        }
+        else
+        {
+            ChangeLayerMask(defaultLayer);
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        ChangeLayerMask(grabLayer);
+        isGrabbed = false;
+        transform.localPosition = drawerTransform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrabbed && drawerTransform != null)
+        {
+            drawerTransform.localPosition = new Vector3(
+                drawerTransform.localPosition.x,
+                drawerTransform.localPosition.y,
+                transform.localPosition.z
+            );
+            CheckLimits();
+        }
+    }
+
+    private void CheckLimits()
+    {
+        // Add your implementation here
+        if (
+            transform.localPosition.x >= limitPosition.x + limitDistances.x
+            || transform.localPosition.x <= limitPosition.x - limitDistances.x
+        )
+        {
+            ChangeLayerMask(defaultLayer);
+        }
+        else if (
+            transform.localPosition.y >= limitPosition.y + limitDistances.y
+            || transform.localPosition.y <= limitPosition.y - limitDistances.y
+        )
+        {
+            ChangeLayerMask(defaultLayer);
+        }
+        else if (drawerTransform.localPosition.z <= limitPosition.z - limitDistances.z)
+        {
+            isGrabbed = false;
+            drawerTransform.localPosition = limitPosition;
+            ChangeLayerMask(defaultLayer);
+        }
+        else if (drawerTransform.localPosition.z >= drawerlimitz + limitDistances.z)
+        {
+            isGrabbed = false;
+            drawerTransform.localPosition = new Vector3(
+                drawerTransform.localPosition.x,
+                drawerTransform.localPosition.y,
+                drawerlimitz
+            );
+            ChangeLayerMask(defaultLayer);
+        }
+    }
+
+    private void ChangeLayerMask(string mask)
+    {
+        interactionLayers = InteractionLayerMask.GetMask(mask);
+    }
+}
